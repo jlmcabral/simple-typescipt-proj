dfb2383bcc67f0ec331a762f9a5e50fb585c3e52
José Cabral <1140471@isep.ipp.pt>
2021-06-08 10:08:48 +0100

    Convert project to ES6 Modules

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,47 +1,45 @@
-/// <reference path="base-component.ts"/>
-/// <reference path="../decorators/autobind.ts"/>
-/// <reference path="../models/drag-and-drop.ts"/>
-/// <reference path="../models/project.ts"/>
+import { Draggable } from '../models/drag-and-drop.js';
+import { Component } from './base-component.js';
+import { Project } from '../models/project.js';
+import { autobind } from '../decorators/autobind.js';
 
-namespace App {
-  // ProjectItem Class
-  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
-    private project: Project;
+// ProjectItem Class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+  private project: Project;
 
-    get persons() {
-      if (this.project.people === 1) {
-        return '1 person';
-      } else {
-        return `${this.project.people} persons`;
-      }
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
     }
+  }
 
-    constructor(hostId: string, project: Project) {
-      super('single-project', hostId, false, project.id);
-      this.project = project;
-      this.configure();
-      this.renderContent();
-    }
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+  }
 
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData('text/plain', this.project.id);
-      event.dataTransfer!.effectAllowed = 'move';
-    }
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
 
-    dragEndHandle(_: DragEvent) {
-      console.log('DragEnd');
-    }
+  dragEndHandle(_: DragEvent) {
+    console.log('DragEnd');
+  }
 
-    configure() {
-      this.element.addEventListener('dragstart', this.dragStartHandler);
-      this.element.addEventListener('dragend', this.dragEndHandle);
-    }
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandle);
+  }
 
-    renderContent() {
-      this.element.querySelector('h2')!.textContent = this.project.title;
-      this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
-      this.element.querySelector('p')!.textContent = this.project.description;
-    }
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+    this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,32 +1,30 @@
-namespace App {
-  // Validation
-  export interface Validatable {
-    value: string | number;
-    required?: boolean;
-    minLenght?: number;
-    maxLength?: number;
-    min?: number;
-    max?: number;
+// Validation
+export interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLenght?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+export function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (validatableInput.minLenght != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLenght;
+  }
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length <= validatableInput.maxLength;
+  }
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
   }
 
-  export function validate(validatableInput: Validatable) {
-    let isValid = true;
-    if (validatableInput.required) {
-      isValid = isValid && validatableInput.value.toString().trim().length !== 0;
-    }
-    if (validatableInput.minLenght != null && typeof validatableInput.value === 'string') {
-      isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLenght;
-    }
-    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
-      isValid = isValid && validatableInput.value.trim().length <= validatableInput.maxLength;
-    }
-    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
-      isValid = isValid && validatableInput.value >= validatableInput.min;
-    }
-    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
-      isValid = isValid && validatableInput.value <= validatableInput.max;
-    }
-
-    return isValid;
-  }
+  return isValid;
 }
